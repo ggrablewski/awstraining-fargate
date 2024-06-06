cd2affce9d93f281da2d8efd918ade12bde09c3d
Grzegorz Grablewski <grzegorz.grablewski@capgemini.com>
2024-06-06 13:55:44 +0200

    Lab 1

--- app/src/main/java/com/awstraining/backend/business/notifyme/NotifyMeService.java
+++ app/src/main/java/com/awstraining/backend/business/notifyme/NotifyMeService.java
@@ -1,9 +1,12 @@
 package com.awstraining.backend.business.notifyme;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotifyMeService {
+
+    private MessageSender messageSender;
 
     // TODO: lab1
     //  1. Inject MessageSender.
@@ -11,9 +14,9 @@ public class NotifyMeService {
     //  1. Inject Translator
     // TODO lab3
     //  1. Inject sentiment detector
-//    @Autowired
-    public NotifyMeService() {
-
+    @Autowired
+    public NotifyMeService(MessageSender messageSender) {
+        this.messageSender = messageSender;
     }
     
     public String notifyMe(NotifyMeDO notifyMe) {
@@ -27,7 +30,9 @@ public class NotifyMeService {
         // TODO: lab3
         //  1. Detect sentiment of translated message.
         //  2. Change sending of text to "setiment: translated text" and return it.
-        return "This service is not available.";
+        final String text = notifyMe.text();
+        messageSender.send(text);
+        return text;
     }
     
 }
--- app/src/main/java/com/awstraining/backend/config/SNSConfig.java
+++ app/src/main/java/com/awstraining/backend/config/SNSConfig.java
@@ -1,6 +1,11 @@
 package com.awstraining.backend.config;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
@@ -8,22 +13,27 @@ import org.springframework.stereotype.Component;
 @Component
 public class SNSConfig {
 
-    @Value("${aws.region}")
-    private String awsRegion;
+   @Value("${aws.region}")
+   private String awsRegion;
 
-    @Value("${aws.accessKey:#{null}}")
-    private String snsAccessKey;
+   @Value("${aws.accessKey:#{null}}")
+   private String snsAccessKey;
 
-    @Value("${aws.secretKey:#{null}}")
-    private String snsSecretKey;
-    
-    // TODO: lab1
-    //  0. Uncomment @Bean section.
-    //  1. Configure AmazonSNS which will be used by fargate within AWS.
-    //  2. Make sure that your task role has access to publish action (ecs-task-role-policy).
-    //  3. Think how to connect with AWS Service from your local pc. 
-//    @Bean
-//    AmazonSNS configureSNSClient() {
-//      
-//    }
+   @Value("${aws.secretKey:#{null}}")
+   private String snsSecretKey;
+
+   // TODO: lab1
+   //  0. Uncomment @Bean section.
+   //  1. Configure AmazonSNS which will be used by fargate within AWS.
+   //  2. Make sure that your task role has access to publish action (ecs-task-role-policy).
+   //  3. Think how to connect with AWS Service from your local pc.
+   @Bean
+   AmazonSNS configureSNSClient() {
+      return ((snsAccessKey != null) && snsSecretKey != null) ?
+         AmazonSNSClientBuilder.standard().build() :
+         AmazonSNSClientBuilder.standard()
+            .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(snsAccessKey, snsSecretKey)))
+            .withRegion(awsRegion)
+            .build();
+   }
 }
